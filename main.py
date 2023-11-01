7c79a8b4c2881da41cfeb71b237171eb5c806245
HisAtri <yz@ghacg.com>
2023-11-01 22:20:04 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+from flask import Flask, request, abort, redirect, send_from_director
+import requests
+
---
+++ mod/mulist.py
@@ -0,0 +1,39 @@
+# 列出文件及元数据
+import mutagen
+import os
+
+
+# Mutagen检查当前文件是否包含图片，没有图片->True
+def no_cover_art(file_path):
+    audio = mutagen.File(file_path)
+    if hasattr(audio, "pictures"):
+        return len(audio.pictures) == 0
+    return True
+
+
+def search_music(path):
+    extensions = ['.mp3', '.wav', '.flac']
+    # 初始化音乐列表
+    musics_without_cover = []
+
+    # 递归遍历：拓展名为上述，且元数据不包含图片
+    def traverse_directory(directory):
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if file.endswith(tuple(extensions)) & no_cover_art(file_path):
+                    audio = mutagen.File(file_path)
+                    musics_without_cover.append({
+                        "path": file_path,
+                        "artist": audio["artist"],
+                        "title": audio["title"],
+                        "album": audio["album"]
+                    })
+
+            # 递归子目录
+            for subdir in dirs:
+                subdir_path = os.path.join(root, subdir)
+                traverse_directory(subdir_path)
+
+    traverse_directory(path)
+    return musics_without_cover
